521f29b72ee5aa53325dafa4975dae9f8ecb1732
Troy Stribling <troy.stribling@gmail.com>
2015-03-28 17:22:41 -0700

    updated extswift

---
+++ FutureLocationTests/LocationManagerTests.swift
@@ -0,0 +1,24 @@
+//
+//  LocationManagerTests.swift
+//  FutureLocation
+//
+//  Created by Troy Stribling on 3/28/15.
+//  Copyright (c) 2015 gnos.us. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import CoreLocation
+import FutureLocation
+
+class LocationManagerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}

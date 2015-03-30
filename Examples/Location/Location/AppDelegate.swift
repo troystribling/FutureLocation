bb681ec928950ae59a98eb05f88ccf9e60daab86
Troy Stribling <troy.stribling@gmail.com>
2015-03-30 08:52:53 -0700

    starting location example

---
+++ Examples/Location/Location/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Location
+//
+//  Created by Troy Stribling on 3/30/15.
+//  Copyright (c) 2015 gnos.us. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+    }
+
+
+}
+
---
+++ Examples/Location/Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Troy Stribling on 3/30/15.
+//  Copyright (c) 2015 gnos.us. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import FutureLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+

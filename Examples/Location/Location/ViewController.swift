cc240b9ecdae5fbd6791b8ae12596dad34209eed
Troy Stribling <troy.stribling@gmail.com>
2015-04-10 08:43:14 -0700

    better handling of location disabled

--- Examples/Location/Location/ViewController.swift
+++ Examples/Location/Location/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UITableViewController {
     @IBOutlet var address3Label      : UILabel!
     @IBOutlet var getAddressButton   : UIButton!
     @IBOutlet var startUpdatesSwitch : UISwitch!
+    @IBOutlet var startUpdatesLabel  : UILabel!
     
     var locationFuture  : FutureStream<[CLLocation]>?
     var addressFuture   : FutureStream<[CLPlacemark]>?
@@ -28,73 +29,73 @@ class ViewController: UITableViewController {
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder:aDecoder)
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.locationManager.distanceFilter = kCLDistanceFilterNone
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        if LocationManager.locationServicesEnabled() {
-            if self.locationManager.isUpdating {
-                self.startUpdatesSwitch.on = true
-            } else {
-                self.startUpdatesSwitch.on = false
-            }
-        }
     }
 
+    override func viewDidAppear(animated: Bool) {
+        if LocationManager.locationServicesEnabled() {
+            if LocationManager.locationServicesEnabled() {
+                if self.locationManager.isUpdating {
+                    self.startUpdatesSwitch.on = true
+                } else {
+                    self.startUpdatesSwitch.on = false
+                }
+            }
+        } else {
+            self.startUpdatesLabel.textColor = UIColor.lightGrayColor()
+            self.startUpdatesSwitch.enabled = false
+            self.getAddressButton.enabled = false
+            self.getAddressButton.setTitleColor(UIColor.lightGrayColor(), forState:UIControlState.Disabled)
+            self.presentViewController(UIAlertController.alertOnErrorWithMessage("Location services disabled"), animated:true, completion:nil)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-
     @IBAction func getAddress(sender:AnyObject) {
-        if LocationManager.locationServicesEnabled() {
-             self.addressFuture = self.addressManager.startUpdatingLocation(10, authorization:.AuthorizedWhenInUse).flatmap {_ -> Future<[CLPlacemark]> in
-                                     self.addressManager.stopUpdatingLocation()
-                                     return self.addressManager.reverseGeocodeLocation()
-                                 }
-            self.addressFuture?.onSuccess {placemarks in
-                if let placemark = placemarks.first {
-                    if let subThoroughfare = placemark.subThoroughfare, thoroughfare = placemark.thoroughfare {
-                        self.address1Label.text = "\(subThoroughfare) \(thoroughfare)"
-                    }
-                    if let subLocality = placemark.subLocality {
-                        self.address2Label.text = "\(placemark.subLocality)"
-                    }
-                    if let subAdministrativeArea = placemark.subAdministrativeArea, administrativeArea = placemark.administrativeArea {
-                        self.address3Label.text = "\(subAdministrativeArea), \(administrativeArea)"
-                    }
+        self.addressFuture = self.addressManager.startUpdatingLocation(10, authorization:.AuthorizedWhenInUse).flatmap {_ -> Future<[CLPlacemark]> in
+                                 self.addressManager.stopUpdatingLocation()
+                                 return self.addressManager.reverseGeocodeLocation()
+                             }
+        self.addressFuture?.onSuccess {placemarks in
+            if let placemark = placemarks.first {
+                if let subThoroughfare = placemark.subThoroughfare, thoroughfare = placemark.thoroughfare {
+                    self.address1Label.text = "\(subThoroughfare) \(thoroughfare)"
+                }
+                if let subLocality = placemark.subLocality {
+                    self.address2Label.text = "\(placemark.subLocality)"
+                }
+                if let subAdministrativeArea = placemark.subAdministrativeArea, administrativeArea = placemark.administrativeArea {
+                    self.address3Label.text = "\(subAdministrativeArea), \(administrativeArea)"
                 }
             }
-            self.addressFuture?.onFailure {error in
-                self.presentViewController(UIAlertController.alertOnError(error), animated:true, completion:nil)
-            }
-        } else {
-            self.presentViewController(UIAlertController.alertOnErrorWithMessage("Location services disabled"), animated:true, completion:nil)
+        }
+        self.addressFuture?.onFailure {error in
+            self.presentViewController(UIAlertController.alertOnError(error), animated:true, completion:nil)
         }
     }
     
     @IBAction func startUpdatingLocation(sender:AnyObject) {
-        if LocationManager.locationServicesEnabled() {
-            if self.locationManager.isUpdating {
-                self.locationManager.stopUpdatingLocation()
-            } else {
-                self.locationFuture = self.locationManager.startUpdatingLocation(10, authorization:.AuthorizedWhenInUse)
-                self.locationFuture?.onSuccess {locations in
-                    if let location = locations.first {
-                        self.latituteLabel.text =  NSString(format: "%.6f", location.coordinate.latitude) as String
-                        self.longitudeLabel.text = NSString(format: "%.6f", location.coordinate.longitude) as String
-                    }
-                }
-                self.locationFuture?.onFailure {error in
-                    self.presentViewController(UIAlertController.alertOnError(error), animated:true, completion:nil)
+        if self.locationManager.isUpdating {
+            self.locationManager.stopUpdatingLocation()
+        } else {
+            self.locationFuture = self.locationManager.startUpdatingLocation(10, authorization:.AuthorizedWhenInUse)
+            self.locationFuture?.onSuccess {locations in
+                if let location = locations.first {
+                    self.latituteLabel.text =  NSString(format: "%.6f", location.coordinate.latitude) as String
+                    self.longitudeLabel.text = NSString(format: "%.6f", location.coordinate.longitude) as String
                 }
             }
-        } else {
-            self.presentViewController(UIAlertController.alertOnErrorWithMessage("Location services disabled"), animated:true, completion:nil)
+            self.locationFuture?.onFailure {error in
+                self.presentViewController(UIAlertController.alertOnError(error), animated:true, completion:nil)
+            }
         }
     }
-
 }
 

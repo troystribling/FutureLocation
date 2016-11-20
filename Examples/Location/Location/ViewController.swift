be9bd93395481ed67e65e7451986c0d9598b8a34
Troy Stribling <troy.stribling@gmail.com>
2016-11-19 20:13:05 -0600

    location example works

--- Examples/Location/Location/ViewController.swift
+++ Examples/Location/Location/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import CoreLocation
 import FutureLocation
-import SimpleFutures
 
 class ViewController: UITableViewController {
 
@@ -22,40 +21,40 @@ class ViewController: UITableViewController {
     @IBOutlet var startUpdatesSwitch: UISwitch!
     @IBOutlet var startUpdatesLabel: UILabel!
     
-    let locationManager = FLLocationManager()
-    let addressManager  = FLLocationManager()
+    let locationManager = LocationManager()
+    let addressManager  = LocationManager()
     
     let addressProgressView = ProgressView()
     let locationProgressView = ProgressView()
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder:aDecoder)
-        self.locationManager.distanceFilter = kCLDistanceFilterNone
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if !self.locationManager.locationServicesEnabled() || self.locationManager.authorizationStatus() == .Denied {
-            self.startUpdatesLabel.textColor = UIColor.lightGrayColor()
-            self.startUpdatesSwitch.enabled = false
-            self.getAddressButton.enabled = false
+        if !locationManager.locationServicesEnabled() || locationManager.authorizationStatus() == .denied {
+            startUpdatesLabel.textColor = UIColor.lightGray
+            startUpdatesSwitch.isEnabled = false
+            getAddressButton.isEnabled = false
             var message = "Location services disabled"
-            if self.locationManager.authorizationStatus() == .Denied {
+            if self.locationManager.authorizationStatus() == .denied {
                 message = "Authorization status is denied"
             }
-            self.getAddressButton.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Disabled)
-            self.presentViewController(UIAlertController.alertOnErrorWithMessage(message), animated: true, completion: nil)
+            getAddressButton.setTitleColor(UIColor.lightGray, for: UIControlState.disabled)
+            present(UIAlertController.alertOnErrorWithMessage(message), animated: true, completion: nil)
         } else {
-            if self.locationManager.locationServicesEnabled() {
-                if self.locationManager.isUpdating {
-                    self.startUpdatesSwitch.on = true
+            if locationManager.locationServicesEnabled() {
+                if locationManager.isUpdating {
+                    startUpdatesSwitch.isOn = true
                 } else {
-                    self.startUpdatesSwitch.on = false
+                    startUpdatesSwitch.isOn = false
                 }
             }
         }
@@ -64,49 +63,49 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
 
-    @IBAction func getAddress(sender: AnyObject) {
+    @IBAction func getAddress(_ sender: AnyObject) {
         self.addressProgressView.show()
-        let addressFuture = self.addressManager.startUpdatingLocation(10, authorization: .AuthorizedWhenInUse).flatmap { [unowned self]  _ -> Future<[CLPlacemark]> in
+        let addressFuture = addressManager.startUpdatingLocation(authorization: .authorizedWhenInUse, capacity: 10).flatMap { [unowned self]  _ -> Future<[CLPlacemark]> in
                                  self.addressManager.stopUpdatingLocation()
                                  return self.addressManager.reverseGeocodeLocation()
                              }
         addressFuture.onSuccess { [unowned self] placemarks in
             if let placemark = placemarks.first {
                 self.addressProgressView.remove()
-                if let subThoroughfare = placemark.subThoroughfare, thoroughfare = placemark.thoroughfare {
+                if let subThoroughfare = placemark.subThoroughfare, let thoroughfare = placemark.thoroughfare {
                     self.address1Label.text = "\(subThoroughfare) \(thoroughfare)"
                 }
                 if let subLocality = placemark.subLocality {
                     self.address2Label.text = "\(subLocality)"
                 }
-                if let subAdministrativeArea = placemark.subAdministrativeArea, administrativeArea = placemark.administrativeArea {
+                if let subAdministrativeArea = placemark.subAdministrativeArea, let administrativeArea = placemark.administrativeArea {
                     self.address3Label.text = "\(subAdministrativeArea), \(administrativeArea)"
                 }
             }
         }
         addressFuture.onFailure { [unowned self] error in
             self.addressProgressView.remove()
-            self.presentViewController(UIAlertController.alertOnError(error), animated: true, completion: nil)
+            self.present(UIAlertController.alertOnError(error), animated: true)
         }
     }
     
-    @IBAction func startUpdatingLocation(sender: AnyObject) {
-        if self.locationManager.isUpdating {
-            self.locationManager.stopUpdatingLocation()
+    @IBAction func startUpdatingLocation(_ sender: AnyObject) {
+        if locationManager.isUpdating {
+            locationManager.stopUpdatingLocation()
         } else {
-            self.locationProgressView.show()
-            let locationFuture = self.locationManager.startUpdatingLocation(10, authorization: .AuthorizedWhenInUse)
-            locationFuture.onSuccess { locations in
+            locationProgressView.show()
+            let locationFuture = locationManager.startUpdatingLocation(authorization: .authorizedWhenInUse, capacity: 10)
+            locationFuture.onSuccess { [unowned self] locations in
                 if let location = locations.first {
                     self.locationProgressView.remove()
                     self.latituteLabel.text =  NSString(format: "%.6f", location.coordinate.latitude) as String
                     self.longitudeLabel.text = NSString(format: "%.6f", location.coordinate.longitude) as String
                 }
             }
-            locationFuture.onFailure { error in
+            locationFuture.onFailure { [unowned self] error in
                 self.locationProgressView.remove()
-                self.startUpdatesSwitch.on = false
-                self.presentViewController(UIAlertController.alertOnError(error), animated: true, completion: nil)
+                self.startUpdatesSwitch.isOn = false
+                self.present(UIAlertController.alertOnError(error), animated: true)
             }
         }
     }
